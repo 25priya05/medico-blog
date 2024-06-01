a478e961c8ea950261c54fa8a40adcbbaf7f1e48
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2024-06-01 15:17:22 +0530

    addchanges

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,8 +1,6 @@
 
 const blogTitleField = document.querySelector('.title');
-const articleField = document.querySelector('.article');
-
-// Banner
+const articleFeild = document.querySelector('.article');
 const bannerImage = document.querySelector('#banner-upload');
 const banner = document.querySelector(".banner");
 let bannerPath;
@@ -12,15 +10,13 @@ const uploadInput = document.querySelector('#image-upload');
 
 bannerImage.addEventListener('change', () => {
     uploadImage(bannerImage, "banner");
-});
-
+})
 uploadInput.addEventListener('change', () => {
     uploadImage(uploadInput, "image");
-});
-
+})
 const uploadImage = (uploadFile, uploadType) => {
     const [file] = uploadFile.files;
-    if (file && file.type.includes("image")) {
+    if(file && file.type.includes("image")){
         const formdata = new FormData();
         formdata.append('image', file);
 
@@ -29,53 +25,14 @@ const uploadImage = (uploadFile, uploadType) => {
             body: formdata
         }).then(res => res.json())
         .then(data => {
-            if (uploadType == "image") {
+            if(uploadType == "image"){
                 addImage(data, file.name);
-            } else {
+            } else{
                 bannerPath = `${location.origin}/${data}`;
                 banner.style.backgroundImage = `url("${bannerPath}")`;
             }
-        });
-     }else {
-        alert("Upload image only");
+        })
+    } else{
+        alert("upload Image only");
     }
-};
-
-const addImage = (imagepath, alt) => {
-    let curPos = articleField.selectionStart;
-    let textToInsert = `\r![${alt}](${imagepath})\r`;
-    articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
-};
-
- let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
- publishBtn.addEventListener('click', () => {
-    if (articleField.value.length && blogTitleField.value.length) {
-       // Generating ID
-        let letters = 'abcdefghijklmnopqrstuvwxyz';
-         let blogTitle = blogTitleField.value.split(" ").join("-");
-        let id = '';
-        for (let i = 0; i < 4; i++) {
-            id += letters[Math.floor(Math.random() * letters.length)];
-         }
-
-       
-        let docName = `${blogTitle}-${id}`;
-         let date = new Date(); // For published at info
-
-        // Access Firestore with db variable
-        db.collection("blogs").doc(docName).set({
-            title: blogTitleField.value,
-            article: articleField.value,
-            bannerImage: bannerPath,
-           publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
-       })
-       .then(() => {
-           console.log('date entered');
-             // location.href = `/${docName}`;
-     })
-         .catch((err) => {
-             console.error(err);
-        });
-     }
- });
+}
