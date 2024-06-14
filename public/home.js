801f5d15c00d32f82b802f12f8a36216128437dd
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2024-06-14 21:02:21 +0530

    addchanges

--- public/home.js
+++ public/home.js
@@ -20,9 +20,10 @@ const loadBlogs = async () => {
 // Function to create a blog card
 const createBlog = (blog) => {
     let data = blog.data();
+    
     blogSection.innerHTML += `
     <div class="blog-card">
-        <img src="./Images/${data.bannerImage}" class="blog-image" alt="">
+        <img src=".${data.bannerImage}" class="blog-image" alt="">
         <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
         <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
         <a href="/${blog.id}" class="btn dark">read</a>
